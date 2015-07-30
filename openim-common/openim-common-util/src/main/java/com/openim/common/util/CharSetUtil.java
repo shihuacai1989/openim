d140d92739358dea1d7770fd6a57bbf366769dad
shihc <shihc@mapbar.com>
2015-07-30 15:34:51 +0800

    框架完善

---
+++ openim-common/openim-common-util/src/main/java/com/openim/common/util/CharSetUtil.java
@@ -0,0 +1,11 @@
+package com.openim.common.util;
+
+import java.nio.charset.Charset;
+
+/**
+ * Created by shihc on 2015/6/17.
+ */
+public class CharsetUtil {
+
+    public static final Charset utf8 = Charset.forName("utf-8");
+}
