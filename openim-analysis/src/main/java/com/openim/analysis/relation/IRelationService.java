28ebbd479c60f3bb29affc4b03087d9039b7bfe9
shihuacai <shihuacai1989@126.com>
2015-09-06 23:45:08 +0800

    错误修复

--- openim-analysis/src/main/java/com/openim/analysis/relation/IRelationService.java
+++ openim-analysis/src/main/java/com/openim/analysis/relation/IRelationService.java
@@ -1,9 +1,14 @@
 package com.openim.analysis.relation;
 
+import java.util.List;
+
 /**
  * Created by shihc on 2015/9/6.
  */
 public interface IRelationService {
-
-
+    /**
+     * 获取二度人脉
+     * @param loginId
+     */
+    List<String> getSecondNetwork(String loginId);
 }
--- openim-analysis/src/main/java/com/openim/analysis/relation/impl/EmbeddedNeo4jRelationServiceImpl.java
+++ openim-analysis/src/main/java/com/openim/analysis/relation/impl/EmbeddedNeo4jRelationServiceImpl.java
@@ -1,20 +1,31 @@
 package com.openim.analysis.relation.impl;
 
 import com.openim.analysis.relation.IRelationService;
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.*;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.neo4j.graphdb.index.Index;
+import org.neo4j.graphdb.traversal.Evaluators;
+import org.neo4j.graphdb.traversal.TraversalDescription;
+import org.neo4j.graphdb.traversal.Traverser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by shihc on 2015/9/6.
  */
 @Service
 public class EmbeddedNeo4jRelationServiceImpl implements IRelationService, InitializingBean, DisposableBean {
+
+    private static final Logger LOG = LoggerFactory.getLogger(EmbeddedNeo4jRelationServiceImpl.class);
+
+    protected static final String LOGIN_ID_FIELD = "loginId";
 
     @Value("${neo4j.db.path}")
     private String dbPath;
@@ -25,15 +36,67 @@ public class EmbeddedNeo4jRelationServiceImpl implements IRelationService, Initi
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        graphDB = new GraphDatabaseFactory().newEmbeddedDatabase(dbPath);
+
     }
 
-    private void connectNeo4j(){
+    private void connectNeo4j() {
+        graphDB = new GraphDatabaseFactory().newEmbeddedDatabase(dbPath);
+        Transaction tx = graphDB.beginTx();
+        try {
+            nodeIndex = graphDB.index().forNodes("nodes");
 
+            //Node startNode = graphDB.createNode();
+            //startNodeId = startNode.getId();
+
+            tx.success();
+        } catch (Exception e) {
+            LOG.error(e.toString());
+        } finally {
+            tx.close();
+        }
     }
 
     @Override
     public void destroy() throws Exception {
+        graphDB.shutdown();
+    }
 
+    @Override
+    public List<String> getSecondNetwork(String loginId) {
+        List<String> secondNetwork = null;
+        Transaction tx = graphDB.beginTx();
+        try {
+            Node node = nodeIndex.get(LOGIN_ID_FIELD, loginId).getSingle();
+
+            TraversalDescription td = graphDB.traversalDescription()
+                    .breadthFirst()
+                    .relationships(RelTypes.KNOWS, Direction.OUTGOING)
+                    .evaluator(Evaluators.excludeStartPosition())
+                            //以下两个限制条件用于查找二度人脉
+                    .evaluator(Evaluators.fromDepth(2))
+                    .evaluator(Evaluators.toDepth(2));
+
+            Traverser traverser = td.traverse(node);
+
+            secondNetwork = new LinkedList<String>();
+            for (Path friendPath : traverser) {
+                secondNetwork.add((String) friendPath.endNode().getProperty(LOGIN_ID_FIELD));
+                /*System.out.println("At depth " + friendPath.length() + " => "
+                        + friendPath.endNode().getProperty(LOGIN_ID_FIELD));*/
+            }
+            tx.success();
+        } catch (Exception e) {
+            LOG.error(e.toString());
+        } finally {
+            tx.close();
+        }
+
+        return secondNetwork;
+
+    }
+
+    protected static enum RelTypes implements RelationshipType {
+        NEO_NODE,
+        KNOWS
     }
 }
