b7b85e0f690c8828d038f3aedc23c7d0d67fb917
shihuacai <shihuacai1989@126.com>
2015-09-14 23:22:25 +0800

    框架完善

--- openim-web/src/main/java/com/lance/RedisSessionConfig.java
+++ openim-web/src/main/java/com/lance/RedisSessionConfig.java
@@ -1,11 +1,17 @@
-package com.openim.manager.configuration;
+package com.lance;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
 @EnableRedisHttpSession
 public class RedisSessionConfig {
+    @Bean
+    public JedisConnectionFactory connectionFactory() {
+        return new JedisConnectionFactory(); // <2>
+    }
     /*@Bean
     public JedisConnectionFactory connectionFactory()
     {
