55e74670431789ad09816f5ba17526ffeb0aa579
shihc <shihc@mapbar.com>
2015-09-18 17:34:04 +0800

    activemq集成完毕

--- openim-common/openim-common-mq/src/main/java/com/openim/common/mq/activemq/package-info.java
+++ openim-common/openim-common-mq/src/main/java/com/openim/common/mq/activemq/package-info.java
@@ -2,5 +2,9 @@
  * Created by shihc on 2015/9/18.
  * activemq的服务，其不同版本对jdk的要有也不相同，所以有时会出现activemq启动失败的情况，注意查看日志即可
  *
+ * queue类型的只能有一个消息消费者.
+ * topic类型的可以有多个消息消费者.
+ * 每个消费者对应一个MessageListener和一个MessageListenerContainer.
+ *
  */
 package com.openim.common.mq.activemq;
