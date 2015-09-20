04de244679400fc2c69294ffec6513d469772289
shihuacai <shihuacai1989@126.com>
2015-09-20 22:05:35 +0800

    计划引入jmx

---
+++ openim-jmx/src/main/java/ChatServerMBean.java
@@ -0,0 +1,7 @@
+/**
+ * Created by shihuacai on 2015/9/20.
+ */
+public class ChatServerMBean{
+    //参考http://www.ibm.com/developerworks/cn/java/j-jtp09196/
+    //http://maimode.iteye.com/blog/1354377
+}
