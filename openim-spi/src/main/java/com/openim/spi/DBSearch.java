a6924d7d63dbc691da29d639331204ca47319128
shihuacai <shihuacai1989@126.com>
2015-09-28 20:39:08 +0800

    错误调整

---
+++ openim-spi/src/main/java/com/openim/spi/DBSearch.java
@@ -0,0 +1,11 @@
+package com.openim.spi;
+
+/**
+ * Created by shihuacai on 2015/9/25.
+ */
+public class DBSearch implements IHello {
+    @Override
+    public String hello(String name) {
+        return "DBSearch";
+    }
+}
---
+++ openim-spi/src/main/java/com/openim/spi/IHello.java
@@ -0,0 +1,8 @@
+package com.openim.spi;
+
+/**
+ * Created by shihuacai on 2015/9/25.
+ */
+public interface IHello {
+    public String hello(String name);
+}
