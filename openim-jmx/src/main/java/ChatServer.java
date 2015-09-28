2724c6459908f92c3bd35ce1decfab31d14ea913
shihuacai <shihuacai1989@126.com>
2015-09-28 23:06:15 +0800

    调整

--- openim-jmx/src/main/java/ChatServer.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by shihuacai on 2015/9/22.
- */
-public class ChatServer {
-}
--- openim-spi/src/main/java/com/openim/spi/IHello.java
+++
@@ -1,8 +0,0 @@
-package com.openim.spi;
-
-/**
- * Created by shihuacai on 2015/9/25.
- */
-public interface IHello {
-    public String hello(String name);
-}
